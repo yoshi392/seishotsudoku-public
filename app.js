3e3018a4fbbad02a61d44688067822cbdbdfab0a
yoshi392 <teruntyo@gmail.com>
2025-12-26 16:48:28 +0900

    Update WORKER_BASE_URL to production URL

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const DATA_URL = "./data/readings.json";
-const WORKER_BASE_URL = "REPLACE_WITH_WORKER_URL";
+const WORKER_BASE_URL = "https://seishotsudoku-push.teruntyo.workers.dev";
 const VAPID_PUBLIC_KEY = "BP51V69QOr3LWj2YhzcVO05ojPb9R_VRiMcNciBxPkOXbBtsYZMuJOxgrpVcr755ixYsWK5hVDJLXSgYpTWfM_I";
 
 function jstTodayKey() {
