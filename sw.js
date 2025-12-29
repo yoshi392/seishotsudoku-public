31279b2d2daec7377ddc3fd980ff9ff238927d28
yoshi392 <teruntyo@gmail.com>
2025-12-29 22:43:45 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "seishotsudoku-pwa-v1";
+const CACHE_NAME = "seishotsudoku-pwa-v4";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
